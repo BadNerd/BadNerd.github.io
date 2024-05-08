6404893efd6739bad9d2d51e3d392ebc8d183bcc
Khoirul Iman <86760043+BadNerd@users.noreply.github.com>
2024-05-08 14:14:01 +0700

    Uploading prototype

---
+++ script.js
@@ -0,0 +1,47 @@
+let calc_btn_dot = document.querySelector(".calc-btn.dot");
+let result_dot = document.querySelector(".result.dot");
+let u_vec_dot = document.querySelector(".u-vector.dot").children; //u vector for dot product
+let v_vec_dot = document.querySelector(".v-vector.dot").children; //v vector for dot product
+
+let calc_btn_cross = document.querySelector(".calc-btn.cross");
+let result_cross = document.querySelector(".result.cross");
+let u_vec_cross = document.querySelector(".u-vector.cross").children; //u vector for cross product
+let v_vec_cross = document.querySelector(".v-vector.cross").children; //v vector for cross product
+
+
+
+
+console.log(calc_btn_dot);
+calc_btn_dot.addEventListener("click", () => {;
+    let u_x = u_vec_dot[1].value;
+    let u_y = u_vec_dot[2].value;
+    let u_z = u_vec_dot[3].value;
+    
+    let v_x = v_vec_dot[1].value;
+    let v_y = v_vec_dot[2].value;
+    let v_z = v_vec_dot[3].value;
+
+    let dot_result = (u_x * v_x) + (u_y * v_y) + (u_z * v_z)
+
+    result_dot.textContent = " = " + u_x + " * " + v_x + " + " + u_y + " * " + v_y + " + " + u_z + " * " + v_z + " = " + dot_result;
+})
+
+
+console.log(calc_btn_cross);
+calc_btn_cross.addEventListener("click", () => {;
+    let u_x = u_vec_cross[1].value;
+    let u_y = u_vec_cross[2].value;
+    let u_z = u_vec_cross[3].value;
+    
+    let v_x = v_vec_cross[1].value;
+    let v_y = v_vec_cross[2].value;
+    let v_z = v_vec_cross[3].value;
+
+    let x_result = (u_y * v_z) - (u_z * v_y);
+    let y_result = (u_z * v_x) - (u_x * v_z);
+    let z_result = (u_x * v_y) - (u_y * v_x);
+
+
+
+    result_cross.textContent = "= [" + x_result +", " + y_result + ", " + z_result +"]";
+})
